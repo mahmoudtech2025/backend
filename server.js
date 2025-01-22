c0345b1c4ce1bac7795cd2152800b6888e8303c1
mahmoudtech2025 <mahmoudtech2025@gmail.com>
2025-01-22 21:20:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(bodyParser.json());
 
 // الاتصال بقاعدة البيانات
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.MONGO_URI=mongodb+srv://mahmoudtech2025:mahmoud2025mahmoud@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority
+, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ تم الاتصال بقاعدة البيانات"))
   .catch((err) => console.error("❌ خطأ في الاتصال بقاعدة البيانات:", err));
 
